90761795afc6d5e7253877a6914e91667d7e69ad
Yacoub Harkati <haryacoub@gmail.com>
2023-01-01 19:26:02 +0100

    fixed overflow bug

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   }, [])
 
   return (
-    <div className="app ">
+    <div className="app overflow-x-hidden">
       <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
